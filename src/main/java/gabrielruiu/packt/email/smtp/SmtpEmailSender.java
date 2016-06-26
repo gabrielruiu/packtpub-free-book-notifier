a0e7d3ce5a31b075a68319b11ab7cfadc5a03628
Gabriel Ruiu <gabriel.ruiu@mail.com>
2016-06-26 13:52:07 +0200

    Adjust SmtpEmailSender to send HTML email, not text

--- src/main/java/gabrielruiu/packt/email/smtp/SmtpEmailSender.java
+++ src/main/java/gabrielruiu/packt/email/smtp/SmtpEmailSender.java
@@ -2,13 +2,20 @@ package gabrielruiu.packt.email.smtp;
 
 import gabrielruiu.packt.email.EmailBodyProvider;
 import gabrielruiu.packt.email.EmailSender;
+import gabrielruiu.packt.exception.EmailPreparationException;
 import gabrielruiu.packt.model.BookSummary;
 import gabrielruiu.packt.properties.EmailProperties;
+import gabrielruiu.packt.scheduler.EmailSubjectResolver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 /**
  * @author Gabriel Mihai Ruiu (gabriel.ruiu@mail.com)
@@ -17,6 +24,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class SmtpEmailSender implements EmailSender {
 
+    private static final Logger LOG = LoggerFactory.getLogger(SmtpEmailSender.class);
+
     @Autowired
     private JavaMailSender javaMailSender;
 
@@ -24,18 +33,31 @@ public class SmtpEmailSender implements EmailSender {
     private EmailBodyProvider emailBodyProvider;
 
     @Autowired
+    private EmailSubjectResolver emailSubjectResolver;
+
+    @Autowired
     private EmailProperties emailProperties;
 
     public void sendEmailWithBookSummary(BookSummary bookSummary) {
-        SimpleMailMessage mail = createEmail(bookSummary);
+        LOG.info(String.format("Sending email with following content: [%s]", bookSummary.toString()));
+        MimeMessage mail = createEmail(bookSummary);
         javaMailSender.send(mail);
+        LOG.info("Email sent");
     }
 
-    private SimpleMailMessage createEmail(BookSummary bookSummary) {
-        SimpleMailMessage mail = new SimpleMailMessage();
-        mail.setTo(emailProperties.getRecipient());
-        mail.setSubject(emailProperties.getSubject());
-        mail.setText(emailBodyProvider.buildEmailBody(bookSummary));
-        return mail;
+    private MimeMessage createEmail(BookSummary bookSummary) {
+        try {
+            MimeMessage mail = javaMailSender.createMimeMessage();
+
+            MimeMessageHelper helper = new MimeMessageHelper(mail, true);
+            helper.setTo(emailProperties.getRecipient());
+            helper.setSubject(emailSubjectResolver.determineEmailSubject(bookSummary.getTitle()));
+            helper.setText(emailBodyProvider.buildEmailBody(bookSummary), true);
+            helper.setFrom(emailProperties.getFrom());
+
+            return mail;
+        } catch (MessagingException e) {
+            throw new EmailPreparationException(bookSummary, e);
+        }
     }
 }
---
+++ src/main/java/gabrielruiu/packt/exception/EmailPreparationException.java
@@ -0,0 +1,13 @@
+package gabrielruiu.packt.exception;
+
+import gabrielruiu.packt.model.BookSummary;
+
+/**
+ * @author Gabriel Mihai Ruiu (gabriel.ruiu@mail.com)
+ */
+public class EmailPreparationException extends RuntimeException {
+
+    public EmailPreparationException(BookSummary bookSummary, Throwable e) {
+        super(String.format("Error while trying to preparing the email with the following content: [%s]", bookSummary.toString()), e);
+    }
+}
